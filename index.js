eff0e6671ec22e42e80623fb1ca056fae9796ce1
Metrime <Michi@Michaels-Mac-mini.local>
2014-02-01 11:37:44 +0100

    Added download progress

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
-var through = require("through2");
+var through = require("through");
 var gutil = require("gulp-util");
 var request = require("request");
+var progress = require("request-progress");
+var col = gutil.colors;
 
-module.exports = function(files){
-
-	var files = typeof files === "string" ? [files] : files;
-
-	var stream = through.obj(function(file,enc,callback){
+module.exports = function(urls){
+	var stream = through(function(file,enc,cb){
 		this.push(file);
-		callback();
+		cb();
 	});
 
-	var downloaded = 0;
 
-	files.forEach(function(file){
-		request(file,function(err,res,body){
-			if(err){
-				gutil.log(gutil.colors.red('Error'), 'in Plugin', '\''+gutil.colors.cyan('gulp-download')+'\'', ': Failed downloading file', gutil.colors.magenta(file), 'skipping download');
-			}else{
-				stream.write(new gutil.File({
-					path:file.split("/").pop(),
-					contents: new Buffer(body)
-				}));
+	var files = typeof urls === 'string' ? [urls] : urls;
+	var downloadCount = 0;
+
+
+	function download(url){
+		var firstLog = true;
+		
+		progress(
+			request({url:url,encoding:null},downloadHandler),
+			{throttle:1000,delay:1000}
+		)
+		.on('progress',function(state){
+			process.stdout.write(' '+state.percent+'%')
+		})
+		.on('data',function(){
+			if(firstLog){
+				process.stdout.write('['+col.green('gulp')+']'+' Downloading '+col.cyan(url)+'...');
+				firstLog = false;
 			}
+		})
 
-			downloaded++;
+		function downloadHandler(err, res, body){
+			var fileName = url.split('/').pop();
+			var file = new gutil.File( {path:fileName, contents: new Buffer(body)} );
+			stream.queue(file);
 
-			if(downloaded === files.length) stream.end();
-		});
-	});
+			process.stdout.write(' '+col.green('Done\n'));
+			downloadCount++;
+			if(downloadCount != files.length){
+				download(files[downloadCount])
+			}else{
+				stream.emit('end')
+			}
+		}
+	}
+	download(files[0])
 
 	return stream;
 }
+
